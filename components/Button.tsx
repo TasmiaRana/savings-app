f28718f905673a76b8c3d664ec9743a123400025
Tasmia Rana <tasmia.1478@gmail.com>
2026-06-06 22:44:46 +0200

    Completed button component with primary, secondary, dashed and disabled variants

---
+++ components/Button.tsx
@@ -0,0 +1,43 @@
+"use client";
+interface ButtonProps {
+  variant?: 'primary' | 'secondary' | 'dashed' | 'disabled';
+  label: string;
+  onClick: () => void; // A basic click handler,
+  icon?: string;
+  iconAlign?: 'left' | 'right';
+}
+
+export default function Button({
+  label,
+  onClick,
+  variant = 'primary',
+  icon,
+  iconAlign = 'left'
+}: ButtonProps) {
+
+  const baseLayout = "w-full py-3.5 rounded-xl text-sm font-semibold gap-1.5 flex items-center justify-center";
+
+  const variants = {
+    primary: "cursor-pointer bg-emerald-600 hover:bg-emerald-500 text-white px-4 py-3 rounded-xl shadow-md shadow-emerald-950/40 flex items-center gap-2 transition tracking-wide", 
+    secondary: "cursor-pointer bg-slate-900 hover:bg-slate-800 border border-slate-800 py-2.5 rounded-xl text-slate-400 flex items-center justify-center gap-1.5 transition",
+    dashed: "cursor-pointer bg-slate-900 hover:bg-slate-800 border border-dashed border-slate-700/60 rounded-xl py-3 text-slate-400 flex items-center justify-center gap-2 transition",
+    disabled: "cursor-not-allowed bg-slate-700 text-slate-400 opacity-50",
+  };
+
+  const combinedClasses = `${baseLayout} ${variants[variant]}`;
+
+
+  return (
+    <button 
+      onClick={onClick}
+      disabled={variant === 'disabled'}
+      className={combinedClasses}>
+
+      {icon && iconAlign === 'left' && <i className={icon}></i>}  
+      {label}
+      {icon && iconAlign === 'right' && <i className={icon}></i>}
+    
+    </button>
+  );
+
+}
