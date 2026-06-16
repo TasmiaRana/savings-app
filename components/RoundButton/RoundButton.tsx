954811117fd1502ba60b22995f4fec8e55904dc2
Tasmia Rana <tasmia.1478@gmail.com>
2026-06-16 21:24:31 +0200

    round button implementation done

---
+++ components/RoundButton/RoundButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { ReactNode } from "react";
+
+interface RoundButtonProps {
+    onClick: () => void;
+    variant: 'primary' | 'secondary';
+    icon?: ReactNode;
+}
+
+export default function RoundButton({
+    onClick,
+    variant,
+    icon
+}: RoundButtonProps) {
+
+    const baseLayout = "w-10 h-10 text-white rounded-full flex items-center justify-center text-slate-400 p-3";
+    const variants = {
+        primary: "bg-slate-700 hover:bg-slate-500 transition",
+        secondary: "border-2 border-slate-700/60 transition hover:bg-slate-700",
+    };
+    const combinedClasses = `${baseLayout} ${variants[variant]}`;
+
+    return (
+        <button className={combinedClasses} onClick={onClick}>
+            {icon}
+        </button>
+    );
+
+}
