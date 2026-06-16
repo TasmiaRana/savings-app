3c0e23c82c6f2ae749a3e2da525ef5c2ce1d1a62
TasmiaRana <74715865+TasmiaRana@users.noreply.github.com>
2026-06-16 21:48:47 +0200

    Round button implemented with primary & secondary variants (#5)
    * Storybook configuration
    
    * Progress bar component completed
    
    * Code review incorporated for Progress bar component
    
    * round button implementation done
    
    * Code review changes for round button
    
    * Code review changes for round button

---
+++ components/RoundButton/RoundButton.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { fn } from 'storybook/test';
+import RoundButton from './RoundButton';
+
+const meta = {
+    title: 'RoundButton',
+    component: RoundButton,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        onClick: fn()
+    },
+    args: { onClick: fn() },
+} satisfies Meta<typeof RoundButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        variant: 'primary',
+        icon: <i className="fa-solid fa-sliders" />,
+        onClick: fn()
+    },
+};
+
+export const Secondary: Story = {
+    args: {
+        variant: 'secondary',
+        icon: <i className="fa-solid fa-xmark" />,
+        onClick: fn()
+    },
+};
+
